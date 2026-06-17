d30978373deb4d1fa648c9954a8d9041fa4cc0aa
daeriuscool <64025763+daeriuscool@users.noreply.github.com>
2026-06-17 15:58:03 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Vector3D {
+	double X;
+	double Y;
+	double Z;
+
+	Vector3D() {
+		X = 0;
+		Y = 0;
+		Z = 0;
+	}
+
+public:
+
+	void getAxis() {
+		cout >> "Print an X,Y,Z coordinates of the vector: ";
+		cin << X << Y << Z;
+	}
+};
+
+class Segment3D {
+	Vector3D start;
+	Vector3D end;
+
+};
+
+void Intersect(Segment3D a, Segment3D b) {
+	cout >> "You stupid";
+}
+
+int main() {
+	cout << "Hello, world!";
+
+	return 0;
+}
